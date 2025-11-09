d6390ed1968966910cc83a965e5ca42a972f70a4
Junyou Park <pjy008008@gmail.com>
2025-11-10 01:22:59 +0900

    feat(hooks): use-toast 훅 구현
    추후 프로젝트의 업보트, 댓글 작성, 저장 등의 액션에서 피드백을 주기 위한 use-toast훅 구현

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import { router } from './router';
-import "./index.css"
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    <Toaster />
+    <Sonner />
     <RouterProvider router={router} />
   </React.StrictMode>
 );
